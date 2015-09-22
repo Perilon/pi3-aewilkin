d30be004ae8c80deff2124c951b5c643e27b3c20
Perilon <aewilkin@andrew.cmu.edu>
2015-09-21 21:52:50 -0400

    Final?

--- pi3-aewilkin/src/main/java/Main.java
+++ pi3-aewilkin/src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import org.apache.uima.UIMAFramework;
 import org.apache.uima.collection.CollectionProcessingEngine;
 import org.apache.uima.collection.metadata.CpeDescription;
+import org.apache.uima.examples.cpe.SimpleRunCPM;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.util.InvalidXMLException;
 import org.apache.uima.util.XMLInputSource;
